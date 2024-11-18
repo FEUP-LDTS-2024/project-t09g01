7dd0bcf3bc78670db535a1fb273c073932c4f902
Maria luiza <up202304306@up.pt>
2024-11-18 18:11:08 +0000

    reorganizing the code

---
+++ src/main/java/com/t09g01/projeto/model/game/arena/Arena.java
@@ -0,0 +1,14 @@
+package com.t09g01.projeto.model.game.arena;
+
+public class Arena {
+    private int width;
+    private int height;
+
+    public Arena(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getHeight() {return height;}
+    public int getWidth() {return width;}
+}
