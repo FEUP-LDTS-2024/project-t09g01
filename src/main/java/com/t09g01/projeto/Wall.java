3f6e242706300c8618175035e910ea406811ccae
Carolina Ferreira <up202303547@up.pt>
2024-11-18 16:57:46 +0000

    Added Class Wall

---
+++ src/main/java/com/t09g01/projeto/Wall.java
@@ -0,0 +1,24 @@
+package com.t09g01.projeto;
+
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.io.IOException;
+
+public class Wall extends Element {
+
+    public Wall(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(getPosition().getX(), getPosition().getY()), "#");
+
+    }
+
+}
