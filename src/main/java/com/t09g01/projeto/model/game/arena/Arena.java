b7d4b32b6db2bd919fbb07f6846a0194938af086
Cuca <up202306850@up.pt>
2024-11-18 19:04:52 +0000

    Floor class implemented

--- src/main/java/com/t09g01/projeto/model/game/arena/Arena.java
+++ src/main/java/com/t09g01/projeto/model/game/arena/Arena.java
@@ -2,9 +2,13 @@ package com.t09g01.projeto.model.game.arena;
 
 import com.t09g01.projeto.model.game.elements.*;
 
+import java.util.List;
+
 public class Arena {
     private int width;
     private int height;
+    private List<Wall> walls;
+    private List<Floor> floors;
 
     public Arena(int width, int height){
         this.width = width;
@@ -19,4 +23,10 @@ public class Arena {
 
     public Watergirl getWatergirl() {return watergirl;}
     public Fireboy getFireboy() {return fireboy;}
+
+    public void setWalls(List<Wall> walls) {this.walls = walls;}
+    public List<Wall> getWalls() {return walls;}
+
+    public void setFloors(List<Floor> floors) {this.floors = floors;}
+    public List<Floor> getFloors() {return floors;}
 }
