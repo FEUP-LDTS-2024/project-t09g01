515d908eb963d4004ba53a137bbde7a19d082e96
Maria luiza <up202304306@up.pt>
2024-11-20 15:10:26 +0000

    added View package with Viewer class that reads the image (of the elements) and draws it on the screen

---
+++ src/main/java/com/t09g01/projeto/view/Viewer.java
@@ -0,0 +1,38 @@
+package com.t09g01.projeto.view;
+
+import com.googlecode.lanterna.TextColor;
+import com.t09g01.projeto.gui.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Viewer {
+
+    BufferedImage image;
+
+    public Viewer(String filepath) throws IOException {
+        URL resource = getClass().getClassLoader().getResource(filepath);
+        assert resource != null;
+        image = ImageIO.read(new File(resource.getFile()));
+    }
+
+    public void draw(GUI gui, int a, int b){                           // a e b representam as coordenadas de início onde a imagem deve ser desenhada na tela
+        for (int y = 0; y < image.getHeight(); y++) {                  // x e y são as coordenadas dos pixels dentro da imagem
+            for (int x = 0; x < image.getWidth(); x++) {
+                int rgb = image.getRGB(x, y);                         // valor do RGB do pixel
+                TextColor.RGB color = getLanternaColor(rgb);          // mapear para cor no Lanterna
+                gui.drawPixel(a + x,b + y, color);              // desenhar o pixel
+            }
+        }
+    }
+
+    public TextColor.RGB getLanternaColor(int rgb){
+        int red = (rgb >> 16) & 0xFF;
+        int green = (rgb >> 8) & 0xFF;
+        int blue = rgb & 0xFF;
+        return new TextColor.RGB(red, green, blue);
+    }
+}
