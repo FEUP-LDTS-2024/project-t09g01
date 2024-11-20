b565d206c2b45e5ecb5d15da59157b6adfc7ea6e
Carolina Ferreira <up202303547@up.pt>
2024-11-20 10:49:33 +0000

    Created ACTION enum and GUI interface with draw methods

---
+++ src/main/java/com/t09g01/projeto/gui/ACTION.java
@@ -0,0 +1,5 @@
+package com.t09g01.projeto.gui;
+
+public enum ACTION {
+    UP, DOWN, LEFT, RIGHT, QUIT, NONE
+}
---
+++ src/main/java/com/t09g01/projeto/gui/GUI.java
@@ -0,0 +1,13 @@
+package com.t09g01.projeto.gui;
+
+import com.t09g01.projeto.model.Position;
+
+public interface GUI {
+
+
+    void drawFireboy(Position position);
+
+    void drawWatergirl(Position position);
+
+    void drawBlock(Position position);
+}
