ffd6dcd9e7eb1c1e734e21c0a55869fdcc74b639
Carolina Ferreira <up202303547@up.pt>
2024-11-20 10:51:47 +0000

    Added clear, refresh and close methods to GUI interface

--- src/main/java/com/t09g01/projeto/gui/GUI.java
+++ src/main/java/com/t09g01/projeto/gui/GUI.java
@@ -2,12 +2,21 @@ package com.t09g01.projeto.gui;
 
 import com.t09g01.projeto.model.Position;
 
+import java.io.IOException;
+
 public interface GUI {
 
+    ACTION getNextAction() throws IOException
 
     void drawFireboy(Position position);
 
     void drawWatergirl(Position position);
 
     void drawBlock(Position position);
+
+    void clear();
+
+    void refresh();
+
+    void close();
 }
