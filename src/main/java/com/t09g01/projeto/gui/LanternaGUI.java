4cd5fe8793a7c92ecdb23fe01ae8568d540b281b
Carolina Ferreira <up202303547@up.pt>
2024-11-20 10:55:32 +0000

    Added LanternaGUI class, yet to be completed

---
+++ src/main/java/com/t09g01/projeto/gui/LanternaGUI.java
@@ -0,0 +1,41 @@
+package com.t09g01.projeto.gui;
+
+import com.t09g01.projeto.model.Position;
+
+public class LanternaGUI implements GUI {
+
+    @Override
+    public ACTION getNextAction() {
+        return null;
+    }
+
+    @Override
+    public void drawFireboy(Position position) {
+    }
+
+    @Override
+    public void drawWatergirl(Position position) {
+
+    }
+
+    @Override
+    public void drawBlock(Position position) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+}
