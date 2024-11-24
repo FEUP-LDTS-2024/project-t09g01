b3c301f94f7951c3dc4ea513a6174db76e786ac8
Maria luiza <up202304306@up.pt>
2024-11-24 12:07:32 +0000

    added states package and State abstract class (following state design pattern)

---
+++ src/main/java/com/t09g01/projeto/states/State.java
@@ -0,0 +1,36 @@
+package com.t09g01.projeto.states;
+
+import com.googlecode.lanterna.screen.Screen;
+import com.t09g01.projeto.Game;
+import com.t09g01.projeto.control.Controller;
+import com.t09g01.projeto.gui.ACTION;
+import com.t09g01.projeto.gui.GUI;
+import com.t09g01.projeto.view.game.ScreenViewer;
+
+import java.io.IOException;
+
+public abstract class State<T> {
+    private final T model;
+    private final Controller<T> controller;
+    private final ScreenViewer<T> screenViewer;
+
+    public State(T model){
+        this.model = model;
+        this.controller = getController();
+        this.screenViewer = getScreenViewer();
+    }
+
+    protected abstract Controller<T> getController();
+    protected abstract ScreenViewer<T> getScreenViewer();
+
+    public T getModel(){
+        return model;
+    }
+
+    public void step(Game game, GUI gui, long time) throws IOException {
+        ACTION action = gui.getNextAction();
+        controller.step(game, action, time);
+        screenViewer.draw(gui);
+    }
+
+}
