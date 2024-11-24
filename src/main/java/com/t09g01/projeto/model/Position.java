e28a201ff7ae4bb7995fd43e56d2187e3d2ad341
Carolina Ferreira <up202303547@up.pt>
2024-11-24 21:33:31 +0000

    Fixed getUp and getDown methods in Position class

--- src/main/java/com/t09g01/projeto/model/Position.java
+++ src/main/java/com/t09g01/projeto/model/Position.java
@@ -13,10 +13,10 @@ public class Position {
     public int getY() {return y;}
 
     public Position getUp() {
-        return new Position(x, y + 1);
+        return new Position(x, y - 1);
     }
     public Position getDown() {
-        return new Position(x, y - 1);
+        return new Position(x, y + 1);
     }
     public Position getRight() {
         return new Position(x + 1, y);
