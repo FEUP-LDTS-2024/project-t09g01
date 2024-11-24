3ca07f74d88ea78757884b3a3aa6d3bd4cb25644
Maria luiza <up202304306@up.pt>
2024-11-24 17:34:46 +0000

    added MenuViewer with its draw method

---
+++ src/main/java/com/t09g01/projeto/view/menu/MenuViewer.java
@@ -0,0 +1,21 @@
+package com.t09g01.projeto.view.menu;
+
+import com.t09g01.projeto.gui.GUI;
+import com.t09g01.projeto.model.Position;
+import com.t09g01.projeto.model.menu.Menu;
+import com.t09g01.projeto.view.game.ScreenViewer;
+
+public class MenuViewer extends ScreenViewer<Menu> {
+    public MenuViewer (Menu menu){super(menu);}
+
+    @Override
+    public void draw(GUI gui) {
+        gui.drawText(new Position(4, 4), "Menu", "#FFFFFF");
+
+        for (int i = 0; i < getModel().getEntriesNumber(); i++) {
+            gui.drawText(new Position(4, 6 + i), getModel().getEntry(i), getModel().isSelected(i) ? "#EDD711" : "#FFFFFF");
+        }
+    }
+
+
+}
