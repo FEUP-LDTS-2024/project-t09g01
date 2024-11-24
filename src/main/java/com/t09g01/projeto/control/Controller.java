8ed14f7e3710795157fe154e5d8db7ddabee0cff
Maria luiza <up202304306@up.pt>
2024-11-24 17:47:11 +0000

    added MenuController with step method

--- src/main/java/com/t09g01/projeto/control/Controller.java
+++ src/main/java/com/t09g01/projeto/control/Controller.java
@@ -3,12 +3,14 @@ package com.t09g01.projeto.control;
 import com.t09g01.projeto.Game;
 import com.t09g01.projeto.gui.ACTION;
 
+import java.io.IOException;
+
 public abstract class Controller<T> {
     private final T model;
     public Controller(T model){
         this.model = model;
     }
     public T getModel(){return model;}
-    public abstract void step(Game game, ACTION action, long time);
+    public abstract void step(Game game, ACTION action, long time) throws IOException;
 
 }
