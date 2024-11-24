c06cdf567f20c5e42e92c814f9e75cfd403ad451
Cuca <up202306850@up.pt>
2024-11-24 11:08:25 +0000

    added WatergirlControllerTest that test movement (up, down, left, right) and step methods, also corrected Position issue with getUp and getDown methods

---
+++ src/test/java/com/t09g01/projeto/control/WatergirlControllerTest.java
@@ -0,0 +1,59 @@
+package com.t09g01.projeto.control;
+
+import com.t09g01.projeto.control.game.WatergirlController;
+import com.t09g01.projeto.gui.ACTION;
+import com.t09g01.projeto.model.Position;
+import com.t09g01.projeto.model.game.elements.Watergirl;
+import com.t09g01.projeto.model.game.temple.Temple;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class WatergirlControllerTest {
+    private Temple temple;
+    private WatergirlController controller;
+    private Watergirl watergirl;
+
+    @BeforeEach
+    void setUp() {
+        temple = Mockito.mock(Temple.class);
+        watergirl = Mockito.mock(Watergirl.class);
+        when(temple.getWatergirl()).thenReturn(watergirl);
+        when(watergirl.getPosition()).thenReturn(new Position(5,5));
+
+        controller = new WatergirlController(temple);
+    }
+
+    @Test
+    void testMoveRight() {
+        controller.moveWatergirlRight();
+        verify(watergirl).setPosition(new Position(6,5));
+    }
+
+    @Test
+    void testMoveLeft() {
+        controller.moveWatergirlLeft();
+        verify(watergirl).setPosition(new Position(4,5));
+    }
+
+    @Test
+    void testMoveUp() {
+        controller.moveWatergirlUp();
+        verify(watergirl).setPosition(new Position(5,6));
+    }
+
+    @Test
+    void testMoveDown() {
+        controller.moveWatergirlDown();
+        verify(watergirl).setPosition(new Position(5,4));
+    }
+
+    @Test
+    void testStep() {
+        controller.step(null, ACTION.WATERGIRL_LEFT, 100);
+        verify(watergirl).setPosition(new Position(4,5));
+    }
+}
