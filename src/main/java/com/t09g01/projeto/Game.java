aaec86598975d4a4ab88be3e6ebb9cd71a2935c4
Maria luiza <up202304306@up.pt>
2024-11-24 12:51:03 +0000

    updated Game to start and run the game correctly

--- src/main/java/com/t09g01/projeto/Game.java
+++ src/main/java/com/t09g01/projeto/Game.java
@@ -1,27 +1,44 @@
 package com.t09g01.projeto;
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
 import com.t09g01.projeto.gui.LanternaGUI;
-import com.t09g01.projeto.model.game.temple.Temple;
-import com.t09g01.projeto.states.GameState;
 import com.t09g01.projeto.states.State;
 
 import java.io.IOException;
 
 public class Game {
-    //private final LanternaGUI gui;
-    //private final State state;
+    private final LanternaGUI gui;
+    private State state;
 
-    public Game() {
-        //this.gui = new LanternaGUI(150, 200);
-        //this.state = new GameState(new Game());
+    public Game() throws IOException {
+        this.gui = new LanternaGUI(640, 360);
+        //this.state = new GameState(new Temple(20, 12));
     }
 
+    public static void main(String[] args) throws IOException {
+        new Game().start();
+    }
+
+    public void setState(State state) {this.state = state;}
+
+    private void start() throws IOException {
+        int FPS = 30;
+        int frameTime = 1000 / FPS;
+
+        while(this.state != null){
+            long startTime = System.currentTimeMillis();
+            state.step(this, gui, startTime);
+
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long sleepTime = frameTime - elapsedTime;
+
+            try {
+                if (sleepTime > 0){
+                    Thread.sleep(sleepTime);
+                }
+            } catch (InterruptedException e) {
+            }
+
+        }
+        gui.close();
+    }
 }
