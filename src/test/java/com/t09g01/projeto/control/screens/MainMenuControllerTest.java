13c21729227f8391e485da89655f4cc6ba33c980
Cuca <up202306850@up.pt>
2024-12-21 20:04:04 +0000

    added tests for MainMenuController: quit and no quit

---
+++ src/test/java/com/t09g01/projeto/control/screens/MainMenuControllerTest.java
@@ -0,0 +1,50 @@
+package com.t09g01.projeto.control.screens;
+
+import com.t09g01.projeto.Game;
+import com.t09g01.projeto.gui.ACTION;
+import com.t09g01.projeto.model.gameover.GameOver;
+import com.t09g01.projeto.model.menu.MainMenu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Set;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class MainMenuControllerTest {
+    private Game game;
+    private MainMenu mainMenu;
+    private EntryController entryController;
+    private MainMenuController mainMenuController;
+
+    @BeforeEach
+    public void setup() {
+        this.game = Mockito.mock(Game.class);
+        this.mainMenu = Mockito.mock(MainMenu.class);
+        this.entryController = Mockito.mock(EntryController.class);
+        this.mainMenuController = new MainMenuController(mainMenu, entryController);
+    }
+
+    @Test
+    public void testQuitAction() throws IOException, URISyntaxException, FontFormatException {
+        Set<ACTION> actions = Set.of(ACTION.QUIT);
+
+        mainMenuController.step(game, actions, System.currentTimeMillis());
+
+        verify(game, times(1)).setState(null);
+    }
+
+    @Test
+    public void testNoQuitAction() throws IOException, URISyntaxException, FontFormatException {
+        Set<ACTION> actions = Set.of(ACTION.NONE);
+
+        mainMenuController.step(game, actions, System.currentTimeMillis());
+
+        verify(game, times(0)).setState(null);
+    }
+}
