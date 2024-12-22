6b12ff6ddd50f161fb235063b52186d5ea3a5589
Cuca <up202306850@up.pt>
2024-12-22 08:04:21 +0000

    Added tests for BlueDiamond initialization

---
+++ src/test/java/com/t09g01/projeto/model/game/elements/BlueDiamondTest.java
@@ -0,0 +1,23 @@
+package com.t09g01.projeto.model.game.elements;
+
+import com.t09g01.projeto.model.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BlueDiamondTest {
+    private BlueDiamond blueDiamond;
+
+    @BeforeEach
+    void setUp() {
+        this.blueDiamond = new BlueDiamond(10, 20);
+    }
+
+    @Test
+    void testBlueDoorInitialization() {
+        Position position = blueDiamond.getPosition();
+        assertEquals(10, position.getX());
+        assertEquals(20, position.getY());
+    }
+}
