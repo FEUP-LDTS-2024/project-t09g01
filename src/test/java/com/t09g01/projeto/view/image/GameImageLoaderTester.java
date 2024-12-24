3ff1a4d492f1b6dd83af84dbc2a5c11c561e4838
Maria luiza <up202304306@up.pt>
2024-12-23 21:24:27 -0300

    added GameImageLoaderTester (100% coverage)

---
+++ src/test/java/com/t09g01/projeto/view/image/GameImageLoaderTester.java
@@ -0,0 +1,39 @@
+package com.t09g01.projeto.view.image;
+
+import com.t09g01.projeto.view.Viewer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class GameImageLoaderTester {
+    private GameImageLoader gameImageLoader;
+
+    @BeforeEach
+    void setUp() {
+        gameImageLoader = new GameImageLoader();
+    }
+
+    @Test
+    void testConstructor() {
+        assertNotNull(gameImageLoader);
+        assertNotNull(gameImageLoader.imageMap);
+    }
+
+    @Test
+    void testGetImage() throws IOException {
+        String filepath = "fonts/font1.png";
+        Viewer viewer = mock(Viewer.class);
+        gameImageLoader.imageMap.put(filepath, viewer);
+
+        Viewer result = gameImageLoader.get(filepath);
+
+        assertNotNull(result);
+        assertEquals(viewer, result);
+        verify(spy(gameImageLoader), never()).get(filepath);
+    }
+
+}
